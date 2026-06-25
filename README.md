ac204232661127e4d6c9faac9d92cd0651374e14
Icosa-dev <ljcottendev@gmail.com>
2026-06-25 16:36:06 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# LDustSim
+
+LDustSim is a GPU-Accelerated N-Body gravitational particle simulation
+built with ILGPU and Raylib.
---
+++ Source/Particle.cs
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2026 LJC
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+using System.Numerics;
+
+namespace LDustSim
+{
+    public struct Particle
+    {
+        public float X, Y;
+        public float VX, VY;
+        public float AX, AY;
+        public float Mass;
+    }
+}
