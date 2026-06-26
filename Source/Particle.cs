9b18da4d702ab5780c811e8251497e76897b96d3
Icosa-dev <ljcottendev@gmail.com>
2026-06-26 02:10:21 -0400

    feat: update Particle

--- Source/Particle.cs
+++ Source/Particle.cs
@@ -1,18 +1,21 @@
 /*
  * Copyright (c) 2026 LJC
- *
+ * 
  * SPDX-License-Identifier: MIT
  */
-
-using System.Numerics;
 
 namespace LDustSim
 {
     public struct Particle
     {
+        // Simulation Attributes
         public float X, Y;
         public float VX, VY;
-        public float AX, AY;
         public float Mass;
+        public bool isGravNode;
+        public bool isMoveable;
+
+        // Rendering Attributes
+        public float Size;
     }
 }
