50676bb1913d25326f80111313f24e839426a2ef
Icosa-dev <ljcottendev@gmail.com>
2026-06-29 16:48:36 -0400

    fix: remove Size attribute from Particle

--- Source/Particle.cs
+++ Source/Particle.cs
@@ -8,14 +8,10 @@ namespace LDustSim
 {
     public struct Particle
     {
-        // Simulation Attributes
         public float X, Y;
         public float VX, VY;
         public float Mass;
         public byte IsGravNode;
         public byte IsMoveable;
-
-        // Rendering Attributes
-        public float Size;
     }
 }
