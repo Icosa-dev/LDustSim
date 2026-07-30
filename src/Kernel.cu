c7e25e30f6e7d00d630ed6d698004284ef2086c2
Icosa-dev <ljcottendev@gmail.com>
2026-07-29 23:06:13 -0400

    feat: move rendering to GPU

--- src/Kernel.cu
+++ src/Kernel.cu
@@ -4,10 +4,11 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <cmath>
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 #include <driver_types.h>
-#include <math.h>
+#include <raylib.h>
 
 #include "Particle.h"
 #include "ParticleSimulator.h"
@@ -62,4 +63,40 @@ void launchSimulationKernel(Particle *currentParticles, Particle *nextParticles,
 
     simulationKernel<<<blocksPerGrid, threadsPerBlock>>>(
         currentParticles, nextParticles, numParticles, gravity, deltaTime);
+}
+
+__global__ void renderParticlesKernel(const Particle *particles,
+                                      int numParticles, Color *pixels,
+                                      int width, int height, float maxSpeed) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (index >= numParticles)
+        return;
+
+    Particle p = particles[index];
+    int x = (int)p.x;
+    int y = (int)p.y;
+
+    if (x >= 0 && x < width && y >= 0 && y < height) {
+        float speed = sqrtf(p.vx * p.vx + p.vy * p.vy);
+        float normalizedSpeed = fminf(fmaxf(speed / maxSpeed, 0.0f), 1.0f);
+
+        Color col = {
+            .r = static_cast<unsigned char>(normalizedSpeed * 255),
+            .g = 0,
+            .b = static_cast<unsigned char>((1.0f - normalizedSpeed) * 255),
+            .a = 255};
+
+        pixels[y * width + x] = col;
+    }
+}
+
+void launchRenderParticlesKernel(const Particle *particles, int numParticles,
+                                 Color *pixels, int width, int height,
+                                 float maxSpeed) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (numParticles + threadsPerBlock - 1) / threadsPerBlock;
+
+    renderParticlesKernel<<<blocksPerGrid, threadsPerBlock>>>(
+        particles, numParticles, pixels, width, height, maxSpeed);
 }
--- src/Kernel.h
+++ src/Kernel.h
@@ -6,7 +6,13 @@
 
 #pragma once
 
+#include <raylib.h>
+
 struct Particle;
 
 void launchSimulationKernel(Particle *currentParticles, Particle *nextParticles,
                             int numParticles, float gravity, float deltaTime);
+
+void launchRenderParticlesKernel(const Particle *particles,
+                                      int numParticles, Color *pixels,
+                                      int width, int height, float maxSpeed);
